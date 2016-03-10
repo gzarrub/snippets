20964219ad7b82cbe75d8925ecad7a099954efd8
Guillermo Zarzuelo Rubio <g.zarrub@gmail.com>
2016-03-11 00:07:56 +0100

    method updated

--- string.py
+++ string.py
@@ -1,34 +1,53 @@
+# -*- coding: utf-8 -*-
 __author__ = 'g.zarrub@gmail.com'
 
 
-def get_title(title, max_length=120, char='#'):
+def center_text(text, length):
+    """Add a padding for each side of the text to complete the selected length
 
-    barrier = ''.join('%s' % char for i in range(max_length))
-    _start_title_ = '%s ' % char
-    _end_title_ = ' %s' % char
-    _title_ = ''
-    message = ''
+    :param text:
+    :param length:
+    :return:
+    """
 
-    def auto_span():
-        length_span1 = (max_length - (len(_start_title_) + len(_end_title_)) - len(_title_))/2
-        span1 = ''.join(' ' for i in range(length_span1))
-        length_span2 = max_length - (len(_start_title_) + len(_end_title_)) - len(_title_) - length_span1
-        span2 = ''.join(' ' for i in range(length_span2))
+    span1 = ''.join(' ' for i in range((length - len(text))/2))
+    span2 = ''.join(' ' for i in range(length - len(text) - len(span1)))
 
-        return span1, span2
+    return '%s%s%s' % (span1, text, span2)
 
-    empty_row = "%s%s%s%s\n" % (_start_title_, auto_span()[0], auto_span()[1], _end_title_)
 
-    message += '\n%s\n%s' % (barrier, empty_row)
+def get_title(text, max_length=120, char='#'):
+    """
 
-    for title_section in title.split(' '):
-        if len(_title_) + len(' %s' % title_section) <= max_length - (len(_start_title_) + len(_end_title_)):
-            _title_ += ' %s' % title_section
+    :param text:
+    :param max_length:
+    :param char:
+    :return:
+    """
+
+    start = '%s ' % char[0]
+    end = ' %s' % char[0]
+    title = row = ''
+
+    title_sections = text.replace('\n', ' ').split(' ')
+    for title_section in title_sections:
+        if len(title_section) > max_length:
+            msg = 'A section of your text is larger than the max_length parameter.'
+            raise ValueError(msg)
+
+    barrier = ''.join('%s' % char[0] for i in range(max_length))
+    empty_row = "%s%s%s\n" % (start, center_text('', max_length - (len(start) + len(end))), end)
+
+    title += '\n%s\n%s' % (barrier, empty_row)
+
+    for title_section in title_sections:
+        if len(row) + len(' %s' % title_section) <= max_length - (len(start) + len(end)):
+            row += ' %s' % title_section
         else:
-            message += '%s%s%s%s%s\n' % (_start_title_, auto_span()[0], _title_, auto_span()[1], _end_title_)
-            _title_ = ''
+            title += '%s%s%s\n' % (start, center_text(row, max_length - (len(start) + len(end))), end)
+            row = ''
 
-    message += '%s%s%s%s%s\n' % (_start_title_, auto_span()[0], _title_, auto_span()[1], _end_title_)
-    message += '%s%s\n' % (empty_row, barrier)
+    title += '%s%s%s\n' % (start, center_text(row, max_length - (len(start) + len(end))), end)
+    title += '%s%s\n' % (empty_row, barrier)
 
-    return message
+    return title
