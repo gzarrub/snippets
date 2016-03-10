3c4da5d428e0d3e7ded03a86f2965450dffe0ebd
Guillermo Zarzuelo Rubio <g.zarrub@gmail.com>
2016-03-10 01:55:25 +0100

    logging basic config and title function

---
+++ logger.py
@@ -0,0 +1,17 @@
+from string import get_title
+import logging.config
+import logging
+import os
+__author__ = 'g.zarrub@gmail.com'
+
+
+CONFIG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'conf', 'logger.conf')
+LOGFILENAME = 'report.log'
+
+logging.config.fileConfig(CONFIG_PATH, {'logfilename': LOGFILENAME}, True)
+
+logging = logging
+
+
+def log_title(title, max_length=120, char='#'):
+    logging.getLogger('default').debug(get_title(title, max_length, char))
