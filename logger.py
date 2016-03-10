e62f3b15870540a706d59f93250fd0737e6a1f0d
Guillermo Zarzuelo Rubio <g.zarrub@gmail.com>
2016-03-11 00:08:16 +0100

    module updated

--- logger.py
+++ logger.py
@@ -1,16 +1,14 @@
+# -*- coding: utf-8 -*-
 from string import get_title
 import logging.config
 import logging
 import os
 __author__ = 'g.zarrub@gmail.com'
 
-
 CONFIG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'conf', 'logger.conf')
 LOGFILENAME = 'report.log'
 
 logging.config.fileConfig(CONFIG_PATH, {'logfilename': LOGFILENAME}, True)
-
-logging = logging
 
 
 def log_title(title, max_length=120, char='#'):
