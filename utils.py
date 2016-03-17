544c81f070492446980e16272614b883609a1cd1
Guillermo Zarzuelo Rubio <g.zarrub@gmail.com>
2016-03-17 20:53:57 +0100

    module renamed

--- utils.py
+++ utils.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
-__author__ = 'gzarzuelo'
+__author__ = 'g.zarrub@gmail.com'
 
 
 def clean_screen():
     os.system("cls") if os.name in ["ce", "nt", "dos"] else os.system("clear")
-
-
-
