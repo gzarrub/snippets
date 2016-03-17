e2ee490a8fb7bd05185640f9433e85ed84f5eb22
Guillermo Zarzuelo Rubio <g.zarrub@gmail.com>
2016-03-18 00:49:30 +0100

    check output file format

--- logging_config.py
+++ logging_config.py
@@ -7,7 +7,6 @@ import os
 __author__ = 'g.zarrub@gmail.com'
 
 
-
 def logging_config(disable_existing_loggers=True):
 
     properties_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config', 'properties.cfg')
@@ -16,6 +15,15 @@ def logging_config(disable_existing_loggers=True):
     config_file = os.path.abspath(config.get('logging', 'config_file'))
     output_file = os.path.abspath(config.get('logging', 'output_file'))
 
+    wrong_output = False
+    if not utils.is_file(output_file):
+        wrong_output = True
+        output_file = os.path.abspath(os.path.join('log', 'default.log'))
+
     utils.safe_path(output_file)
 
     logging.config.fileConfig(config_file, {'logfilename': output_file}, disable_existing_loggers)
+
+    if wrong_output:
+        msg = 'The output file defined at %s has a not allowed format, changed to %s' % (config_file, output_file)
+        logging.getLogger(__name__).warning(msg)
