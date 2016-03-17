198822cbc45781a5cca94e58d085185473357cc5
Guillermo Zarzuelo Rubio <g.zarrub@gmail.com>
2016-03-17 23:50:03 +0100

    logging properties read from properties file

--- logging_config.py
+++ logging_config.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
+import extended_config
 import logging.config
 import logging
+import utils
 import os
 __author__ = 'g.zarrub@gmail.com'
 
-DEFAULT_CONFIG_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config', 'logging.cfg')
-DEFAULT_OUTPUT_FILE = 'report.log'
 
 
-def logging_config(config=DEFAULT_CONFIG_FILE, output=DEFAULT_OUTPUT_FILE, disable_existing_loggers=True):
+def logging_config(disable_existing_loggers=True):
 
-    logging.config.fileConfig(config, {'logfilename': output}, disable_existing_loggers)
+    properties_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config', 'properties.cfg')
+    config = extended_config.ExtendedConfig().read(properties_file)
+
+    config_file = os.path.abspath(config.get('logging', 'config_file'))
+    output_file = os.path.abspath(config.get('logging', 'output_file'))
+
+    utils.safe_path(output_file)
+
+    logging.config.fileConfig(config_file, {'logfilename': output_file}, disable_existing_loggers)
