efab48fa87c708c2d754e4da7de504b6b8161f21
Guillermo Zarzuelo Rubio <g.zarrub@gmail.com>
2016-03-20 02:54:08 +0100

    includes modified

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from logging_config import logging_config
-from menu import Menu
+import snippets.extended_config.extended_config as extended_config
+import snippets.logging_config.logging_config as logging_config
+import snippets.menu_builder.menu_builder as menu_builder
+import snippets.helpers.helpers as helpers
 __author__ = 'g.zarrub@gmail.com'
-
-
-logging_config()
